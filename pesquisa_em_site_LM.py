77d5955a5cf0b1c0fd366d7df5755539ab270299
Emerson Viçosa <emersonvicosa@gmail.com>
2023-07-31 19:26:37 -0300

    avanço no programa; possível pesquisar por LM

--- pesquisa_em_site_LM.py
+++ pesquisa_em_site_LM.py
@@ -1,17 +1,23 @@
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
+#Chrome_options__
 chrome_options = Options()
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 chrome_options.add_experimental_option("detach", True)
-servico = Service(ChromeDriverManager().install())
-navegador = webdriver.Chrome(service=servico, options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 
-navegador.get("https://www.ahnegao.com.br/")
-navegador.maximize_window()
+#get_url__
+driver.get('https://www.leroymerlin.com.br/')
+driver.implicitly_wait(10)
 
-links = navegador.find_element("xpath", "//a[@href]")
+#find_url__
+search1 = driver.find_element(By.XPATH,
+     "//*[contains(@class, 'aE8V9') and contains(@class, 'htek1') and contains(@class, 'C0tWl')]")
+search1.send_keys('91697550')
+search1.send_keys(Keys.ENTER)
+  
 
 
-navegador.quit()
