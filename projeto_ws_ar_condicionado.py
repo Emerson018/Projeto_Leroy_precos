6d9e6c9f010632d93ca67c76c51e3610f71315fe
Emerson Viçosa <emersonvicosa@gmail.com>
2023-08-11 23:48:01 -0300

    copiei do 'melh...cod...' para main,pois tava bug

--- projeto_ws_ar_condicionado.py
+++ projeto_ws_ar_condicionado.py
@@ -6,13 +6,22 @@ import csv
 import datetime
 import os
 
+#functions__
+def verifica_preco(preco_salvo, preco_atual):
+    if preco_salvo != preco_atual:
+        print('Houve uma alterção no preço!')
+    else:
+        print('O preço continua o mesmo.')
+
 #variables__
 palavra_chave = 'const'
 contador = 0
-valores = []
+valor_real = []
+valor_centavo = []
 linhas_texto = ''
 ean_13 = ''
-padrao = r'\d+\.\d{3}|\d,\d{2}'
+padrao_real = r'\d+\.\d{3}|\d.\d{2}'
+padrao_centavo = r'.\d{2}'
 data_hora = datetime.datetime.now()
 nome_arquivo = f"dados_{data_hora.strftime('%Y%m%d_%H%M%S')}.csv"
 
@@ -25,19 +34,20 @@ soup = BeautifulSoup(html_content, "html.parser")
 
 #data_get___
 prod_barcode = soup.find('div', class_ = 'badge product-code badge-product-code').text
+
+#+++++encontra apenas os valores do lm ou do cod. barra+++++++
 for caractere in prod_barcode:
     if caractere.isdigit():
         ean_13 += caractere
+#=====================
 
-prod_title = soup.find('h1', class_ = 'product-title align-left color-text').text.strip()
+prod_title = soup.find('h1', class_ = 'product-title align-left color-text').text.replace('\n', '')
 
 prod_price = soup.find('div', class_= 'product-price-tag')
 
-#==== A PARTIR DAQUI MODIFICR O PROD_PRICE PRA DIMINUIR A QUANTIDADE DE CARACTERES====
-
 #Write_archive__
-with open(nome_arquivo, 'w', newline='') as arquivo:
-    writer = csv.writer(arquivo)
+with open(nome_arquivo, 'w', newline='') as file:
+    writer = csv.writer(file)
     writer.writerows(prod_price)
 
 #Read_archive__
@@ -45,34 +55,62 @@ with open(nome_arquivo, 'r') as file:
     reader = csv.reader(file)
 
     for row in reader:
-        linhas_texto = linhas_texto + ','.join(row) + '\n'
+        linhas_texto = linhas_texto + '.'.join(row) + '\n'
 
 #remove_archive__
 os.remove(nome_arquivo)
 
-#find_price__
+#find_real__
 for linha in linhas_texto.split('\n'):
     if palavra_chave in linha:
-        match = re.search(padrao, linha)
+        match = re.search(padrao_real, linha)
         if match:
             preco = match.group()
-            valores.append(preco)
-            contador += 1
-            if contador >=1:
+            valor_real.append(preco)
+            contador += 2
+            if contador >=2:
                 break
 
-#print_price__
-price =','.join(valores)
+#centavo
+for linha in linhas_texto.split('\n'):
+    if palavra_chave in linha:
+        match = re.search(padrao_centavo, linha)
+        if match:
+            preco = match.group()
+            valor_centavo.append(preco)
+            contador += 1
+            if contador >=4:
+                centavos = valor_centavo[1]
+                break
+
+#adjust_price__ 
+preco_atual =','.join(valor_real)
+preco_atual = (preco_atual + centavos).replace('.', ',')
+preco_antigo =','.join(valor_real)
+preco_antigo = (preco_antigo + centavos).replace('.',',')
+
+with open(nome_arquivo, 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow([preco_antigo])
+
+with open(nome_arquivo, 'r') as file:
+    preco_salvo = file.read().strip().replace('"','')
+
+os.remove(nome_arquivo)
 
 product = {'LM': [ean_13],
         'Title': [prod_title],
-        'Price': [price]}
+        'Price': [preco_atual]}
 
 dados = pd.DataFrame(product)
-dados.to_csv('produto.csv', index= False, encoding='utf-8', sep=';')
+dados.to_csv('teste.csv', index= False, encoding= 'utf-8', sep= ';')
 
-print("Arquivo criado!!!")
-print(price)
+print(f'Código: {ean_13}\n'
+       f'Título: {prod_title}\n'
+       f'Preco atual: {preco_atual}\n'
+       f'Preco antigo: {preco_salvo}')
+
+verifica_preco(preco_atual, preco_salvo)
 '''
 todos sao do tipo STR
 print(prod_barcode) 
