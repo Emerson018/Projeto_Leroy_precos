d4caa97631fc0e5fde9eb4f5800d93d714696eb6
Emerson Viçosa <emersonvicosa@gmail.com>
2023-09-15 23:03:26 -0300

    identando e encontrando mais infos

--- teste_find_all_prices.py
+++ teste_find_all_prices.py
@@ -5,24 +5,30 @@ import csv
 import datetime
 import os
 
+
 def ajusta_info(infos):
 
-    informacao = infos.find_next('td').text
+    if infos is not None:
+        informacao = infos.find_next('td').text
+
+    else:
+        informacao = '---'
 
     return print(informacao)
+
 
 def find_price(prod_price):
 
     linhas_texto = ''
     data_hora = datetime.datetime.now()
     nome_arquivo = f"dados_{data_hora.strftime('%Y%m%d_%H%M%S')}.csv"
-    
-    #Write_archive__
+
+    # Write_archive__
     with open(nome_arquivo, 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(prod_price)
 
-    #Read_archive__
+    # Read_archive__
     with open(nome_arquivo, 'r') as file:
         reader = csv.reader(file)
 
@@ -34,45 +40,70 @@ def find_price(prod_price):
     values = []
 
     for line in linhas_texto.split('\n'):
-            match = re.search(default, line)
-            if match:
-                price = match.group()
-                values.append(price)
-            
+        match = re.search(default, line)
+        if match:
+            price = match.group()
+            values.append(price)
 
     elemento = values[0]
     valor = elemento.split("'")[1]
-
 
     os.remove(nome_arquivo)
 
     return valor
 
-#app_action___
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
+
+# app_action___
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
 url = "https://www.leroymerlin.com.br/banheira-de-imersao-zen-150x72cm-branco-sensea_91989296"
-req = requests.get(url,headers=headers)
+
+req = requests.get(
+    url,
+    headers=headers
+)
+
 html_content = req.text
-soup = BeautifulSoup(html_content, "html.parser")
+soup = BeautifulSoup(
+    html_content,
+    "html.parser"
+)
 
-prod_price = soup.find('div', class_= 'product-price-tag')
-span_tag = soup.find('div', {'data-product-pice-tag'})
+prod_price = soup.find(
+    'div',
+    class_='product-price-tag'
+)
 
-info = soup.find('div', class_= 'product-info-details')
+span_tag = soup.find(
+    'div',
+    {'data-product-pice-tag'}
+)
+
+info = soup.find(
+    'div',
+    class_='product-info-details'
+)
 
 
-infos_produto = ['Produto', 'Dimensão', 'Cor','Modelo','Marca', 'Garantia do Fabricante']
+infos_produto = [
+    'Produto',
+    'Dimensão',
+    'Cor',
+    'Modelo',
+    'Marca',
+    'Garantia do Fabricante'
+    'teste'
+]
 
-#ajusta a informação dos produtos para cada dado contido dentro da lista informações
-#dos produtos se os dados não forem None.
-dados = [ajusta_info(info.find('th',string=dado)) for dado in infos_produto if dado is not None]
-
+# ajusta a informação dos produtos para cada dado contido dentro da lista informações
+# dos produtos se os dados não forem None.
+dados = [ajusta_info(info.find('th', string=dado)) for dado in infos_produto]
 
 
 linhas_texto = find_price(prod_price)
 
 print(linhas_texto)
 
-#DESCOBRIR O QUE SAO
-#API
-#FRAMEWORK
+# DESCOBRIR O QUE SAO
+# API
+# FRAMEWORK
--- testes_antes_da_main/edit_antes_da_main.py
+++ testes_antes_da_main/edit_antes_da_main.py
@@ -6,13 +6,15 @@ import csv
 import datetime
 import os
 
-#functions__
+# functions__
+
 
 def add_values_to_csv(dados, nome_arquivo):
 
     with open(nome_arquivo, 'a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(dados)
+
 
 def add_values_to_excel(dados):
     dados = pd.DataFrame(product)
@@ -21,38 +23,44 @@ def add_values_to_excel(dados):
     existing_lm_values = df1['LM'].dropna().tolist()
 
     new_lm_values = dados['LM'].tolist()
-    values_to_add = [lm for lm in new_lm_values if lm not in existing_lm_values]
+    values_to_add = [
+        lm for lm in new_lm_values if lm not in existing_lm_values]
 
     if values_to_add:
         dados_to_add = dados[dados['LM'].isin(values_to_add)]
         with pd.ExcelWriter(
-                            'teste.xlsx',
-                            mode='a',
-                            engine= 'openpyxl',
-                            if_sheet_exists='overlay'
-                            ) as writer:
-            
+            'teste.xlsx',
+            mode='a',
+            engine='openpyxl',
+            if_sheet_exists='overlay'
+        ) as writer:
+
             dados_to_add.to_excel(
-                            writer,
-                            sheet_name='Sheet1',
-                            header= None,
-                            startrow=writer.sheets['Sheet1'].max_row,
-                            index=False
-                            )
+                writer,
+                sheet_name='Sheet1',
+                header=None,
+                startrow=writer.sheets['Sheet1'].max_row,
+                index=False
+            )
+
 
 def format_real(text_lines):
-    
+
     default = r"const integers = '([\d.]+)'"
     values = []
 
     for line in text_lines.split('\n'):
-        match = re.search(default, line)
+        match = re.search(
+            default,
+            line
+        )
         if match:
             price = match.group()
             values.append(price)
-               
+
     value = values[0].split("'")[1]
     return value
+
 
 def format_cents(text_lines):
     key_word = 'const'
@@ -62,29 +70,33 @@ def format_cents(text_lines):
 
     for line in text_lines.split('\n'):
         if key_word in line:
-            match = re.search(default, line)
+            match = re.search(
+                default,
+                line
+            )
             if match:
                 price = match.group()
                 values.append(price)
-                counter +=1
-                if counter >=4:
+                counter += 1
+                if counter >= 4:
                     cents_value = values[1]
                     break
-    
+
     return cents_value
 
-def find_price(prod_price): 
+
+def find_price(prod_price):
 
     linhas_texto = ''
     data_hora = datetime.datetime.now()
     nome_arquivo = f"dados_{data_hora.strftime('%Y%m%d_%H%M%S')}.csv"
-    
-    #Write_archive__
+
+    # Write_archive__
     with open(nome_arquivo, 'w', newline='') as file:
         writer = csv.writer(file)
         writer.writerows(prod_price)
 
-    #Read_archive__
+    # Read_archive__
     with open(nome_arquivo, 'r') as file:
         reader = csv.reader(file)
 
@@ -95,53 +107,96 @@ def find_price(prod_price):
 
     return linhas_texto
 
+
 def data_get(soup):
     ean_13 = ''
     nome_arquivo_csv = "dados.csv"
 
-    prod_barcode = soup.find('div', class_ = 'badge product-code badge-product-code').text
+    prod_barcode = soup.find(
+        'div',
+        class_='badge product-code badge-product-code'
+    ).text
+
     for caractere in prod_barcode:
         if caractere.isdigit():
             ean_13 += caractere
 
-    title = soup.find('h1', class_ = 'product-title align-left color-text').text.replace('\n', '')
+    title = soup.find(
+        'h1',
+        class_='product-title align-left color-text'
+    ).text.replace('\n', '')
 
-    prod_price = soup.find('div', class_= 'product-price-tag')
+    prod_price = soup.find(
+        'div',
+        class_='product-price-tag'
+    )
+    infos = soup.find(
+        'div',
+        class_='product-info-details'
+    )
 
-    return nome_arquivo_csv, title, prod_price, ean_13
+    infos_produto = [
+    'Produto',
+    'Dimensão',
+    'Cor',
+    'Modelo',
+    'Marca',
+    'Garantia do Fabricante'
+    'teste',
+    'Tipo',
+    'Potencia',
+    'Tipo de Ar Condicionado']
+    
+    return nome_arquivo_csv, title, prod_price, ean_13, infos, infos_produto
+
 
 def format_data(reais, centavos):
-    #format_price__ 
+    # format_price__
     preco = (reais + centavos)
 
-    #format_data__
+    # format_data__
     product = {'LM': [str(ean_13)],
-            'Title': [str(title)],
-            'Price': [preco]}
+               'Title': [str(title)],
+               'Price': [preco]}
     produtos_csv = [ean_13, title, preco]
 
     return product, produtos_csv, preco
 
+
+def ajusta_info(infos):
+    if infos is not None:
+        informacao = infos.find_next('td').text
+    else:
+        informacao = '---'
+
+    return print(informacao)
+
 def main():
-    #app_action___
+    # app_action___
     url = input('Digite o link: ')
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
-    #url = https://www.leroymerlin.com.br/ar-condicionado-split-24000-btus-quente-e-frio-220v-series-a1-tcl_91697550?term=91697550&searchTerm=91697550&searchType=LM
-    req = requests.get(url,headers=headers)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'}
+    # url = https://www.leroymerlin.com.br/ar-condicionado-split-24000-btus-quente-e-frio-220v-series-a1-tcl_91697550?term=91697550&searchTerm=91697550&searchType=LM
+    req = requests.get(
+        url,
+        headers=headers
+    )
     html_content = req.text
-    soup = BeautifulSoup(html_content, "html.parser")
-    
+    soup = BeautifulSoup(
+        html_content,
+        "html.parser"
+    )
     return soup
 
 
-#call_functions__
+# call_functions__
 soup = main()
-nome_arquivo_csv, title, prod_price, ean_13 = data_get(soup)
+nome_arquivo_csv, title, prod_price, ean_13, infos, infos_produto = data_get(soup)
 linhas_texto = find_price(prod_price)
 reais = format_real(linhas_texto)
 centavos = format_cents(linhas_texto)
 product, produtos_csv, preco = format_data(reais, centavos)
-#save_data__
+# save_data__
 add_values_to_excel(product)
 add_values_to_csv(produtos_csv, nome_arquivo_csv)
 
@@ -151,20 +206,12 @@ print(
     f'Título: {title}\n'
     f'Preco atual: R${preco}')
 
+dados = [ajusta_info(infos.find('th', string=dado)) for dado in infos_produto]
+
 # só nao ta encontrando valroes abaixo de 10 reais
 
 '''
 caracteristicas para buscar:
 
-tipo
-garantia
-produto
-marca
-modelo
-cor
-profundidade
-comprimeito
-altura
-potencia
-'''
 
+'''
